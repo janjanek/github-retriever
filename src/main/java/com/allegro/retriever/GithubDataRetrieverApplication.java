465d00b406386fb2b23a142fe796dbbf9a83ca67
janjanek <jas.star@wp.pl>
2021-04-23 13:04:46 +0200

    Initial commit

---
+++ src/main/java/com/allegro/retriever/GithubDataRetrieverApplication.java
@@ -0,0 +1,14 @@
+package com.allegro.retriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDataRetrieverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubDataRetrieverApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/allegro/retriever/GithubDataRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allegro.retriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDataRetrieverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
