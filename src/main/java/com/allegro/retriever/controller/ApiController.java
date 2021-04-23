9883a355dff9a81dcdeebe02d8e21b088cfaa752
janjanek <jas.star@wp.pl>
2021-04-23 13:13:45 +0200

    Rest Controller class creation.

---
+++ src/main/java/com/allegro/retriever/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.allegro.retriever.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public String MainApi(){
+        return "Hello main!";
+    }
+}
