917f48f267425483d29e235fccab014f34dc3de7
janjanek <jas.star@wp.pl>
2021-04-23 21:41:31 +0200

    Add controller to retrieve data from github api.

--- src/main/java/com/allegro/retriever/controller/ApiController.java
+++ src/main/java/com/allegro/retriever/controller/ApiController.java
@@ -1,5 +1,8 @@
 package com.allegro.retriever.controller;
 
+import com.allegro.retriever.domain.Repos;
+import com.allegro.retriever.service.JsonClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +11,12 @@ public class ApiController {
 
     private static final String DATA_URL = "https://api.github.com/users/allegro/repos?per_page=100";
 
+    @Autowired
+    private JsonClientService clientService;
 
-    @GetMapping("/")
-    public String MainApi() {
-        return "Hello main!";
+    @GetMapping("/user/repos")
+    public Repos MainApi() {                                //may add later throws NoSuchFieldException
+
+        return clientService.parseList(DATA_URL);
     }
 }
